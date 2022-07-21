23c9334a98c61b88d1d862da9f2f3577dbdcb1cd
Sibusiso Dlamini <g20d9300@campus.ru.ac.za>
2022-07-21 14:52:12 +0200

    completed task1;

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,25 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+
+    }
+
+    public int Task1(List<Integer> a, List<Integer> b) {
+
+        int result = 0;
+        for (int i = 0; i < b.size() - 1; i++) {
+
+            int index = b.get(i);
+
+            // b.get(index);
+
+            if (index < a.size() - 1 && index >= 0) {
+                result = result + a.get(index);
+            }
+
+        };
+
+        return result;
+    }
 }
