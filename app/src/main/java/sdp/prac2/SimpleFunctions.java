eb920d65ccec541f8efd5bdacecc70cdcf17f2f5
Oratile <oratzzile@gmail.com>
2022-07-21 16:02:32 +0200

    Task 5 added

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,20 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static Boolean Task5(List<Integer> T) {
+        List<Integer> test  = new ArrayList<Integer>();
+        int len = T.size();
+        for (int i = 0; i < len; i++) {
+            test.add(T.get(i));
+        }
+        Collections.sort(T); 
+        for (int i = 0; i < len; i++) {
+            if (T.get(i) != test.get(i)) {
+                return false;
+            }
+        }
+        return true; 
+    }
+    
 }
