ed3c73722fc01ba218c4488aa4c00dd52dd7ab43
Tim <timwilliams9810@gmail.com>
2022-07-24 20:30:31 +0200

    task 6 completed

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -39,4 +39,26 @@ public class SimpleFunctions {
         }
         return true; 
     }
+
+    /*  take numbers from list, round them up the next 
+        hundred and insert them into a new list and return it */
+    public static ArrayList Task6 (ArrayList<Integer> list){
+        ArrayList newList = new ArrayList<Integer>();
+        int i = 0;
+        while (i < list.size()){
+            int num = list.get(i);
+            int checkHundred = num % 100; //returns 0 if it was a multiple of 100
+            if (checkHundred == 0){
+                newList.add(num);
+            } else if (num < 100) {
+                newList.add(100);   // numbers below 100 will have 100 to a multiple 
+            } else{
+                int roundUp = num / 100;
+                roundUp = roundUp * 100 + 100;
+                newList.add(roundUp);
+            }           
+            i++;
+        }
+        return newList;
+    }
 }
